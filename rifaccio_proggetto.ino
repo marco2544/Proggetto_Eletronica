8ab6fb30c56bca38f7004f058f0607bbdba8ea42
marco2544 <129094321+marco2544@users.noreply.github.com>
2026-03-25 12:19:23 +0100

    Add files via upload

--- rifaccio_proggetto.ino
+++ rifaccio_proggetto.ino
@@ -7,14 +7,23 @@
 //libreria accelerometro
 #include <DFRobot_BMI160.h>
 
+//struttura campionamento accelerometro
+typedef struct{
+  float x;
+  float y;
+  float z;
+  unsigned long temp;
+}Accelerazione;
+
 //creo l'oggetto accelerometro (per poter ricavare le informazioni da esso)
 DFRobot_BMI160 bmi160;
-
-const int8_t i2c_addr = 0x69;
 //creo l'oggetto servomotore (per dare i comandi)
 Servo myServo;
 //creo l'oggetto per le preferences per salvare in memoria le variabili (funziona come una sorta di hasmap)
 Preferences preferences;
+
+//indirizzo seriale i2c
+const int8_t i2c_addr = 0x69;
 
 //constanti pin sensori
 //pin per il servo
@@ -23,6 +32,7 @@ const int pinServo  = 14;
 //costanti  varie per la gestione degli input
 const String EXIT = "ESC";
 const String FINE = "FINE";
+const int N_CAMPIONAMENTI = 10;
 
 // costante per la conversione in °/s 
 float gyroScale = 2000.0 / 32768.0; 
@@ -30,18 +40,15 @@ float gyroScale = 2000.0 / 32768.0;
 //variabili per l'accelerometro
 int8_t rslt;
 int16_t accelGyro[6] = {0};
+Accelerazione accelerezioni[N_CAMPIONAMENTI];
+ 
 
 //variabile per il servo memoriza la posizione del servomotore
 float currentPosition = 0.0;
 
-const int CAMPIONAMENTO = 50;
-unsigned long stop = 0;
-unsigned long intervallo = 0;
-
-
 
 //matrice per i tempi espressi ms per i ms che il servo impiega a fare 1 grado a quella velocità
-float msPerDegreeTable[10] = { 24, 20, 16, 12, 10, 8, 6, 5, 4, 3 };
+float msPerDegreeTable[10] = { 1000, 20, 16, 12, 10, 8, 6, 5, 4, 3 };
 
 //velocità impostate del servomotore
 //                      1     2     3     4     5    6     7     8     9     10
@@ -282,16 +289,25 @@ void ruota(int degrees, int speed){
   float totalTime = msPerDeg * gradi;
 
   //avvio la rotazione e acquisisco i dati dell'accelerometro
+  int i=0;
   myServo.writeMicroseconds(pwmTable[velDiRotazione]);
-  stop=millis()+totalTime;
+  unsigned long stop = millis()+totalTime;
+  unsigned long intervallo = 0;
+  unsigned long start = millis();
+  float campionamento = totalTime/N_CAMPIONAMENTI;
   while(millis()<stop){
     intervallo=millis();
-    acc_val();
-    while(intervallo+CAMPIONAMENTO>millis()){
+    acc_val(i,intervallo-start);
+    i++;
+    while(intervallo+campionamento>millis()){
       //aspetto per un giusto camionamento
     }
   }
-
+  max();
+  min();
+  media();
+  //da chiedere se la vuole visualizare
+  stampaArray();
   // Ferma il servo
   myServo.writeMicroseconds(1500);
 
@@ -306,7 +322,7 @@ void ruota(int degrees, int speed){
 }
 
 
-void acc_val(){
+void acc_val(int i, float time){
   //acqioisisco i dati dell'accelerometro nel mio array
   bmi160.getAccelGyroData(accelGyro);
 
@@ -314,11 +330,77 @@ void acc_val(){
   float gx = accelGyro[0] * gyroScale;
   float gy = accelGyro[1] * gyroScale;
   float gz = accelGyro[2] * gyroScale;
-
+  accelerezioni[i].x=accelGyro[0] * gyroScale;
+  accelerezioni[i].y=accelGyro[1] * gyroScale;
+  accelerezioni[i].z=accelGyro[2] * gyroScale;
+  accelerezioni[i].temp=time;
 
   // stampo val giroscopio in °/s
+  //debug
+  /*
   Serial.print("Gyro X: "); Serial.print(gx); Serial.print(" °/s  ");
   Serial.print("Y: "); Serial.print(gy); Serial.print(" °/s  ");
   Serial.print("Z: "); Serial.println(gz);
+  */
   
+  
+}
+
+void max(){
+  Accelerazione max[3]={0};
+  for(Accelerazione a: accelerezioni){
+    if(fabs(max[0].x)<fabs(a.x)){
+      max[0]=a;
+    }
+    if(fabs(max[1].y)<fabs(a.y)){
+      max[1]=a;
+    }
+    if(fabs(max[2].z)<fabs(a.z)){
+      max[2]=a;
+    }
+  }
+  Serial.println("\nmax x:");
+  stampaStruct(max[0]);//stampa la struct di max rispetto x
+  Serial.println("max y:");
+  stampaStruct(max[1]);//stampa la struct di max rispetto y
+  Serial.println("max z:");
+  stampaStruct(max[2]);//stampa la struct di max rispetto z
+}
+void min(){
+  Accelerazione min[3]={accelerezioni[0],accelerezioni[0],accelerezioni[0]};
+  for(Accelerazione a: accelerezioni){
+    if(fabs(min[0].x)>fabs(a.x)){
+      min[0]=a;
+    }
+    if(fabs(min[1].y)>fabs(a.y)){
+      min[1]=a;
+    }
+    if(fabs(min[2].z)>fabs(a.z)){
+      min[2]=a;
+    }
+  }
+  Serial.println("\n min x:");
+  stampaStruct(min[0]);//stampa la struct di max rispetto x
+  Serial.println("min y:");
+  stampaStruct(min[1]);//stampa la struct di max rispetto y
+  Serial.println("min z:");
+  stampaStruct(min[2]);//stampa la struct di max rispetto z
+}
+  
+void media(){
+  
+}
+
+void stampaArray(){
+  for(int i=0;i<N_CAMPIONAMENTI;i++){
+    Serial.print("lettura: ");Serial.print(i);
+    stampaStruct(accelerezioni[i]);
+  }
+}
+
+void stampaStruct(Accelerazione a){
+  Serial.print("X: "); Serial.print(a.x); Serial.print(" °/s  ");
+  Serial.print("Y: "); Serial.print(a.y); Serial.print(" °/s  ");
+  Serial.print("Z: "); Serial.print(a.z);Serial.print(" °/s  ");
+  Serial.print("temp:");Serial.print(a.temp);Serial.println(" ms ");
 }
